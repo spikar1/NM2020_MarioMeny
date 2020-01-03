e70fd5e100b9a0e003b2a52e8b850b4aac0ffd2a
Steffen Fureli <steffen.fureli@gmail.com>
2020-01-03 11:14:47 +0100

    Init
    ialize

---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region PlayerStats
+    public float maxSpeed = 1;
+    public float jumpHeight = 1;
+    public float acceleration = 1;
+    #endregion
+
+    Rigidbody2D rb;
+
+
+    void FixedUpdate() {
+        float horInput = 0;
+        bool jumpInput = Input.GetButtonDown("Jump");
+
+        //TODO: Needs support for multiple players
+        horInput = Input.GetAxisRaw("Horizontal");
+
+        Move(horInput);
+        if (jumpInput) {
+            Jump();
+        }
+    }
+
+    private void Move(float direction) {
+
+    }
+
+    private void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+}

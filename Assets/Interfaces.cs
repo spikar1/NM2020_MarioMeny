1a908197d4076e9b42800370a0af9ff2aa0bcf2a
Steffen Fureli <steffen.fureli@gmail.com>
2020-01-03 12:48:07 +0100

    Added Interfaces class
    An implemented it on Player

---
+++ Assets/Interfaces.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public interface IBumpable
+{
+    void Bumped(Player bumpee);
+}
+
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IBumpable
 {
     #region PlayerStats
     public float maxSpeed = 1;
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
 
     Rigidbody2D rb;
 
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     void FixedUpdate() {
         float horInput = 0;
@@ -28,10 +31,28 @@ public class Player : MonoBehaviour
     }
 
     private void Move(float direction) {
-
+        rb.velocity = new Vector2(direction * maxSpeed, rb.velocity.y);
     }
 
     private void Jump() {
         rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+
+    public void Bumped(Player other) {
+        print(name + " is bumped by " + other);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+
+        Transform other = collision.collider.transform;
+        float otherY = other.position.y;
+        IBumpable bumpable = other.GetComponent<IBumpable>();
+        if (bumpable == null)
+            return;
+
+
+        if(otherY > transform.position.y && rb.velocity.y > 0) {
+            bumpable.Bumped(this);
+        }
     }
 }

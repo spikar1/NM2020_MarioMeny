5e947b6dbbb5367c083011fe866dcfc25e15287a
Roland <rolandk@hotmail.no>
2020-01-03 12:52:53 +0100

    Added SelectionBox Spawner
    This is the first version and not complete (Will not work)

---
+++ Assets/Roland/Scripts/BoxSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSpawner : MonoBehaviour
+{
+    public GameObject boxPrefab;
+    public float spawnTime;
+
+    void Start()
+    {
+        StartCoroutine(SpawnBox());
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, Vector3.one);
+        Gizmos.DrawSphere(transform.position, 0.2f);
+    }
+
+    IEnumerator SpawnBox()
+    {
+        //Wait given time before doing anything:
+        yield return new WaitForSeconds(spawnTime);
+
+        //Start Particle Effect:
+
+
+        //Spawn Selection Box:
+        Instantiate(boxPrefab, transform.position, Quaternion.identity);
+
+        //Destroy This Spawner:
+        Destroy(gameObject);
+    }
+}

6c08777da0edca58034618167c4b180c6d634de6
Roland <rolandk@hotmail.no>
2020-01-04 16:52:11 +0100

    Added Timer
    First Version

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Countdown : MonoBehaviour
+{
+    private TextMeshProUGUI textMesh;
+    int min;
+    int sec;
+
+    public Vector2 startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponentInChildren<TextMeshProUGUI>();
+        min = (int)startTime.x;
+        sec = (int)startTime.y;
+    }
+
+    float t = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        sec = (int)t;
+
+        if(sec > 0)
+        {
+            //sec -= Time.deltaTime;
+        }
+
+        textMesh.text = $"{min}:{sec}";
+    }
+}

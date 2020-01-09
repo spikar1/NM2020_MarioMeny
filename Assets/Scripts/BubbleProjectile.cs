dae1e908cf5f5ad627f9465636429f9a5d2c9742
Steffen Fureli <steffen.fureli@gmail.com>
2020-01-09 18:33:41 +0100

    more bubbly fixes

--- Assets/Scripts/BubbleProjectile.cs
+++ Assets/Scripts/BubbleProjectile.cs
@@ -6,9 +6,14 @@ public class BubbleProjectile : Projectile
 {
     public override void OnTriggerEnter2D(Collider2D collision)
     {
+        Bubble bubble = collision.GetComponent<Bubble>();
+        if (bubble)
+            Destroy(gameObject);
+
         Player otherPlayer = collision.GetComponent<Player>();
         if (!otherPlayer)
             otherPlayer = collision.transform.parent.GetComponent<Player>();
+
         if (otherPlayer)
             Instantiate(Manager.WorldOptions.bubblePrefab, otherPlayer.transform.position + Vector3.up * .5f, Quaternion.identity);
 

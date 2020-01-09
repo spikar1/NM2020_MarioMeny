d15d25eef4b485b05b03c9582d141b30ca7e02a0
Steffen Fureli <steffen.fureli@gmail.com>
2020-01-09 10:10:04 +0100

    Added a BounceTrigger Script
    Needs a trigger collider on the gameObject

---
+++ Assets/Scripts/BounceTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class BounceTrigger : MonoBehaviour
+{
+    public float bounceHeight = 10;
+
+    private void OnTriggerStay2D(Collider2D collision) {
+        Player p = collision.GetComponent<Player>();
+
+        if (!p)
+            return;
+        var rb = p.GetComponent<Rigidbody2D>();
+
+        rb.velocity = new Vector2(rb.velocity.x, bounceHeight);
+    }
+}

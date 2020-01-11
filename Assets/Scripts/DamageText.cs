5ac07f36e604c7898f35acafbd41e205ab07982b
Roland <rolandk@hotmail.no>
2020-01-11 01:11:42 +0100

    Updated Damage And Stock Display

---
+++ Assets/Scripts/DamageText.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageText : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position -= new Vector3(0, 25, 0) * Time.deltaTime;
+    }
+}
--- Assets/Scripts/StockCanvas.cs
+++ Assets/Scripts/StockCanvas.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class StockCanvas : MonoBehaviour
 {
     public TextMeshProUGUI[] textPros;
+    public GameObject[] slots;
+    public GameObject textPrefab;
 
     static StockCanvas instance = null;
     private void Awake()
@@ -28,19 +31,71 @@ public class StockCanvas : MonoBehaviour
 
     public void PlayerJoined(int playerNumber, int playerStock)
     {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if(i == playerNumber - 1)
+            {
+                slots[i].SetActive(true);
+                var tex = slots[i].GetComponentInChildren<TextMeshProUGUI>();
+
+                tex.text = $"P{playerNumber}:\n{playerStock}";
+                tex.color = Manager.WorldOptions.playerColors[playerNumber - 1];
+
+                Image[] images = slots[i].GetComponentsInChildren<Image>();
+                foreach (Image image in images)
+                {
+                    if(image.gameObject.transform.parent.transform.parent != null)
+                    {
+                        image.color = Manager.WorldOptions.playerColors[playerNumber - 1];
+                    }
+                }
+            }
+        }
+
+        //Old:
+        /*
         for (int i = 0; i < textPros.Length; i++)
         {
             if(i == playerNumber - 1)
             {
-                textPros[i].enabled = true;
+                textPros[i].gameObject.SetActive(true);
                 textPros[i].text = $"P{playerNumber}:\n{playerStock}";
                 textPros[i].color = Manager.WorldOptions.playerColors[playerNumber - 1];
             }
-        }
+        }*/
     }
 
     public void UpdatePlayerStock(int playerNumber, int playerStock)
     {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i == playerNumber - 1)
+            {
+                var tex = slots[i].GetComponentInChildren<TextMeshProUGUI>();
+
+                if (playerStock > 0)
+                {
+                    tex.text = $"P{playerNumber}:\n{playerStock}";
+                    Image[] images = slots[i].GetComponentsInChildren<Image>();
+                    foreach (Image image in images)
+                    {
+                        if (image.gameObject.transform.parent.transform.parent != null)
+                        {
+                            StartCoroutine(DamageImage(image, 0.3f, slots[i], 1f));
+                        }
+                    }
+
+                }
+                else
+                {
+                    tex.text = $"P{playerNumber}:\nDEAD";
+                }
+            }
+        }
+
+
+        //Old:
+        /*
         for (int i = 0; i < textPros.Length; i++)
         {
             if(i == playerNumber - 1)
@@ -54,20 +109,43 @@ public class StockCanvas : MonoBehaviour
                     textPros[i].text = $"P{playerNumber}:\nDEAD";
                 }
             }
-        }
+        }*/
     }
 
     public void DisablePlayerStocks()
     {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            slots[i].SetActive(false);
+        }
+
+        //old:
+        /*
         for (int i = 0; i < textPros.Length; i++)
         {
-            textPros[i].enabled = false;
-        }
+            textPros[i].gameObject.SetActive(false);
+        }*/
     }
 
     public void SelfDestruct()
     {
         Destroy(gameObject);
+    }
+
+    IEnumerator DamageImage(Image image, float damageTime, GameObject parent, float textTime)
+    {
+        Color startColor = image.color;
+        GameObject clone = Instantiate(textPrefab, parent.transform.position + new Vector3(40,-20,0), Quaternion.identity);
+        clone.transform.parent = parent.transform;
+        clone.GetComponent<TextMeshProUGUI>().color = startColor;
+
+
+        image.color = Color.white;
+        yield return new WaitForSeconds(damageTime);
+        image.color = startColor;
+
+        yield return new WaitForSeconds(textTime);
+        Destroy(clone);
     }
 
 }
